dea54fefbfee62212aa503465e09cf350d254b6b
ccdany-founder <ccdany@outlook.com>
2014-02-18 15:24:29 +0800

    profilehostholder impl

--- src/main/java/com/renren/profile/web/access/ProfileHostHolderImpl.java
+++ src/main/java/com/renren/profile/web/access/ProfileHostHolderImpl.java
@@ -4,9 +4,11 @@ import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.InvocationLocal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.renren.profile.biz.model.User;
 
+@Component
 public class ProfileHostHolderImpl implements ProfileHostHolder {
 
     private static final String KEY_CUR_USER = "__current_user__";
