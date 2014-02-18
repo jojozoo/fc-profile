7397ad8b19447edc589b52364e6912308e9a828f
ccdany-founder <ccdany@outlook.com>
2014-02-18 15:08:31 +0800

    类名错误

--- src/main/java/com/renren/profile/web/access/ProfileHostHolderImpl.java
+++ src/main/java/com/renren/profile/web/access/ProfileHostHolderImpl.java
@@ -4,12 +4,9 @@ import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.InvocationLocal;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import com.renren.profile.biz.model.User;
 
-@Service
 public class ProfileHostHolderImpl implements ProfileHostHolder {
 
     private static final String KEY_CUR_USER = "__current_user__";
@@ -36,5 +33,4 @@ public class ProfileHostHolderImpl implements ProfileHostHolder {
     public void setUser(User user) {
         inv.getRequest().setAttribute(KEY_CUR_USER, user);
     }
-
 }
